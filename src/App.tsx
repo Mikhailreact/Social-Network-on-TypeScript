1ad95393f409af174cccf1d3b9dd8e5ef8c42a1f
Grinchenko Mikhail <grinchenco.01@mail.ru>
2020-11-11 18:15:05 +0300

    Lesson10

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
-
 import './App.css';
+import {Technologies} from "./Technologies";
+import {Header} from "./Header";
+import {Footer} from "./Footer";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
---
+++ src/Technologies.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Technologies = () => {
+    return (
+        <div>
+            <h1>Путь Самурая Реак TypeScript</h1>
+            <p>Привет Павел!</p>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
+}
